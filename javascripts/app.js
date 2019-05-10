ce146edbf2e02e547a726618afeb45443ec0d3ee
Juan Vicente Vazquez G <juanvicentevazquezg@gmail.com>
2019-05-10 20:58:49 +0200

    Finished text mode

--- javascripts/app.js
+++ javascripts/app.js
@@ -11,14 +11,14 @@ const rover = {
 let grid = new Array(10);
 let rovers = [];
 rover.travelLog.push(rover.x, rover.y, rover.direction); //Travel log of initial position
-const compassCW = "NESW"; //Clockwise   //Añadir al fichero principal
-const compassACW= "WSEN"; //Anti ClockWise; //Añadir al fichero principal
-
+const compassCW = "NESW"; //Clockwise   
 let maxObstacleNumber = 4;
 let maxRoverGenerator = 4;
 
 initGrid();
 grid[rover.y][rover.x] = 2; //This number represents our rover. Put our rover on the grid
+
+console.log (`Our Rover starts at the position ${rover.x},${rover.y} and facing to ${rover.direction}`);
 roverGenerator(maxRoverGenerator);
 
 command(
@@ -40,22 +40,26 @@ function command(lc) {
   console.log(`This is the List of commands introduced: ${lc}`);
   for (let i = 0; i < lc.length; i++) {
     if (lc[i] === "R" || lc[i] === "F" || lc[i] === "L" || lc[i] === "B") {
-      console.log("");
-      console.log("Our Rover say: ");
+      
+      console.log("\nOur Rover say: ");
       movementDecisionOrTurn(lc[i], rover, 2);
       rover.travelLog.push(rover.x, rover.y, rover.direction);
     }
     for (let j = 0; j < maxRoverGenerator; j++) {
       //Movement Rover Generator
-      console.log("");
-      console.log(`Rover number ${j + 2} say:`);
+     
+      console.log(`\nRover number ${j + 2} say:`);
       movementDecisionOrTurn(movementRovers(), rovers[j], j + 3);
       rovers[j].travelLog.push(rovers[j].x, rovers[j].y, rovers[j].direction);
     }
-    console.log(grid);
+  
   }
 }
 function movementDecisionOrTurn(lc, roverMars, id) {
+let canMove=false;
+let copyX = roverMars.x; //make a copy of last position.
+let copyY = roverMars.y;
+console.log (`My position now is ${roverMars.x},${roverMars.y}.`)
   switch (lc) {
     case "R":
       turnRover(lc, roverMars);
@@ -63,139 +67,83 @@ function movementDecisionOrTurn(lc, roverMars, id) {
     case "L":
       turnRover(lc, roverMars);
       break;
-    case "F":
-      moveRover(lc, roverMars, id);
+    case "F":canMove=moveForward(roverMars);
       break;
-    case "B":
-      moveRover(lc, roverMars, id);
+    case "B":canMove=moveBackward(roverMars);
       break;
   }
-}
+  if (canMove){
+    console.log(`Rover is facing ${roverMars.direction}`);
+    console.log(`New position of rover is ${roverMars.x},${roverMars.y}`);
+    grid[copyY][copyX] = 0; //Left this position; 0 means there aren't nothing in this position
 
-function moveRover(forwardNBackward, roverMars, id) {
-  let copyX = roverMars.x;
-  let copyY = roverMars.y;
- 
-  switch (roverMars.direction) {
-    case "N":
-      {
-        if (
-          (roverMars.y === 0 && forwardNBackward === "F") ||
-          (roverMars.y === 9 && forwardNBackward === "B")
-        ) {
-          {
-            console.log("I'm sorry but I can not get out of the grid");
-            return false;
-          }
-        } else if (forwardNBackward === "F") {
-          if (iCanMove(roverMars.x, roverMars.y - 1)) {
-            roverMars.y--;
-          } else {
-            return false;
-          }
-        } else if (forwardNBackward === "B") {
-          if (iCanMove(roverMars.x, roverMars.y + 1)) {
-            roverMars.y++;
-          } else {
-            return false;
-          }
-        }
-      }
-      break;
-    case "E":
-      {
-        if (
-          (roverMars.x === 9 && forwardNBackward === "F") ||
-          (roverMars.x === 0 && forwardNBackward === "B")
-        ) {
-          {
-            console.log("I'm sorry but I can not get out of the grid");
-            return false;
-          }
-        } else if (forwardNBackward === "F") {
-          if (iCanMove(roverMars.x + 1, roverMars.y)) {
-            roverMars.x++;
-          } else {
-            return false;
-          }
-        } else if (forwardNBackward === "B") {
-          if (iCanMove(roverMars.x - 1, roverMars.y)) {
-            roverMars.x--;
-          } else {
-            return false;
-          }
-        }
-      }
-      break;
-    case "S":
-      {
-        if (
-          (roverMars.y === 9 && forwardNBackward === "F") ||
-          (roverMars.y === 0 && forwardNBackward === "B")
-        ) {
-          {
-            console.log("I'm sorry but I can not get out of the grid");
-            return false;
-          }
-        } else if (forwardNBackward === "F") {
-          if (iCanMove(roverMars.x, roverMars.y + 1)) {
-            roverMars.y++;
-          } else {
-            return false;
-          }
-        } else if (forwardNBackward === "B") {
-          if (iCanMove(roverMars.x, roverMars.y - 1)) {
-            roverMars.y--;
-          } else {
-            return false;
-          }
-        }
-      }
-      break;
-    case "w":
+    //Marks the new position with none free mark
+    grid[roverMars.y][roverMars.x] = id; //number 2 is de intern name of our rover  because others rovers have other number 0 no obstacle 1 a obstacle 2 first rover 3 other rover ....
+  }
+}
+function moveForward(rover) {
+
+ if (moveRover(rover,"F")) {
+   switch (rover.direction){
+     case "N":{rover.y--}break;
+     case "S":{rover.y++}break;
+     case "E":{rover.x++}break;
+     case "W":{rover.x--}break;
+   }
+
+   console.log("moveForward was called");
+   return true;
+ }
+
+else return false;
+}
+function moveBackward(rover) {
+  let copyX = rover.x; //make a copy of last position.
+  let copyY = rover.y;
+  if (moveRover(rover,"B")){
+    switch (rover.direction){
+      case "N":{rover.y++}break;
+      case "S":{rover.y--}break;
+      case "E":{rover.x--}break;
+      case "W":{rover.x++}break;
+    }
+    console.log("moveBackward was called");
+    return true;
+  }
+
+else return false;
+}
+function moveRover (roverMars,toWhere){
+  
+  switch (roverMars.direction){
+    case "N":{if (toWhere==="F") {return iCanMove(roverMars.x,roverMars.y-1);}
+              else {return iCanMove(roverMars.x,roverMars.y+1);}
+              }break;
+    case "S":{if (toWhere==="F") {return iCanMove(roverMars.x,roverMars.y+1);}
+              else {return iCanMove(roverMars.x,roverMars.y-1);}
+              }break;
+    case "E":{if (toWhere==="F") {return iCanMove(roverMars.x+1,roverMars.y);}
+              else {return iCanMove(roverMars.x-1,roverMars.y);}
+              }break;
+    case "W":{if (toWhere==="F") {return iCanMove(roverMars.x-1,roverMars.y);}
+              else {return iCanMove(roverMars.x+1,roverMars.y);}
+              }break;
+  }
+}
+function iCanMove(x, y) { 
+if (x<0 || y<0 || x>9 || y > 9 ) {
+  console.log("I'm sorry but I can not get out of the grid");
+  return false;
+}
+switch (grid[y][x])
     {
-      if (
-        (roverMars.x === 0 && forwardNBackward === "F") ||
-        (roverMars.x === 9 && forwardNBackward === "B")
-      ) {
-        {
-          console.log("I'm sorry but I can not get out of the grid");
-          return false;
-        }
-      } else if (forwardNBackward === "F") {
-        if (iCanMove(roverMars.x - 1, roverMars.y)) {
-          roverMars.x--;
-        } else {
-          return false;
-        }
-      } else if (forwardNBackward === "B") {
-        if (iCanMove(roverMars.x + 1, roverMars.y)) {
-          roverMars.x++;
-        } else {
-          return false;
-        }
-      }
-    }      break;
+      case 0:{return true}break;//I don't know if is needed this last "break"
+      case 1:{console.log(`Sorry I can't move to this position (${x},${y})  because I have found an obstacle`); return false;}break;
+      case 2:{console.log(`Sorry I can't move to this position (${x},${y})  because I have found the mars-rover`); return false;}break;
+      default:{console.log(`Sorry I can't move to this position (${x},${y})  because I have found the rover number ${grid[y][x]-1}`); return false;}break;
+    }
   }
- 
-  console.log(`Last position was ${copyX},${copyY}`);
-  console.log(`Rover is facing ${roverMars.direction}`);
-  console.log(`New position of rover is ${roverMars.x},${roverMars.y}`);
-  if (forwardNBackward === "F") {
-    moveForward(roverMars);
-  } else {
-    moveBackward(roverMars);
-  }
-
-  //Marks the old position with free mark
-
-  grid[copyY][copyX] = 0; //Left this position; 0 means there aren't nothing in this position
-
-  //Marks the new position with none free mark
-  grid[roverMars.y][roverMars.x] = id; //number 2 is de intern name of rover  because others rovers have other number 0 no obstacle 1 a obstacle 2 first rover 3 other rover ....
-}
-function movementRovers() {
-  //The movemments or turns are random
+function movementRovers() { //The movemments or turns are random
   let movementOrTurn = Math.floor(Math.random() * 2); //Decide Randomly if makes  a turn or movement
   if (movementOrTurn === 0) {
     //With 0 makes a turn
@@ -209,7 +157,6 @@ function movementRovers() {
     } else return "B";
   }
 }
-
 function turnRover(turn, roverMars) {
   //To move Rover turn de Left or Right;
   if (turn === "R") {
@@ -221,14 +168,7 @@ function turnRover(turn, roverMars) {
 function turnLeft(rover) {
   console.log(`Rover is facing ${rover.direction} and turns Left`);
   let index =compassCW.indexOf(rover.direction);
-  /*let index;
-  let compass;
-
-  if (rover.direction==="W" || rover.direction==="S" ) {compass = compassACW;}
-  else{ compass = compassACW;}
-
-  index=compass.indexOf(rover.direction);
-*/
+  
   if (index == 0) {
     index = 3;
   } else {
@@ -242,12 +182,7 @@ function turnLeft(rover) {
 function turnRight(rover) {
   console.log(`Rover is facing ${rover.direction} and turns Right`);
   let index;
-  /*let compass;
-
-  if (rover.direction==="W" || rover.direction==="S" ) {compass = compassCW;}
-  else{ compass = compassACW;}*/
-
-  index=compassCW.indexOf(rover.direction);
+    index=compassCW.indexOf(rover.direction);
 
   if (index == 3) {
     index = 0;
@@ -258,24 +193,6 @@ function turnRight(rover) {
   console.log(`Rover now is facing ${rover.direction}`);
   console.log("turnRight was called!");
 }
-function moveForward(rover) {
-  console.log("moveForward was called");
-}
-function moveBackward(rover) {
-  console.log("moveBackward was called");
-}
-
-
-function iCanMove(x, y) {  //Añadir al fichero principal
-    switch (grid[y][x])
-    {
-      case 0:{return true}break;//I don't know if is needed this last "break"
-      case 1:{console.log(`Sorry I can't move to this position (${x},${y})  because I have found an obstacle`); return false;}break;
-      case 2:{console.log(`Sorry I can't move to this position (${x},${y})  because I have found the mars-rover`); return false;}break;
-      default:{console.log(`Sorry I can't move to this position (${x},${y})  because I have found the rover number ${grid[y][x]-1}`); return false;}break;
-    }
-  }
-  
 function initGrid() {
   //Is ok
   for (var i = 0; i < grid.length; i++) {
@@ -289,9 +206,6 @@ function initGrid() {
 
   obstacleGenerator(maxObstacleNumber); //Numbers of obstacles created ramdomly
 }
-
-
-
 function obstacleGenerator(number) {
   //Is ok
   let x = 0;
@@ -304,7 +218,6 @@ function obstacleGenerator(number) {
     grid[y][x] = 1; //This number represent obstacles
   }
 }
-
 function roverGenerator(number) {
   //Is ok
   let rgeneratorX;
@@ -325,12 +238,14 @@ function roverGenerator(number) {
     });
     rovers[i].travelLog.push(rovers[i].x, rovers[i].y, rovers[i].direction);
   }
+  rovers.forEach((item,index)=>{console.log (`Rover number ${index+2} starts at the position ${item.x},${item.y} and facing to ${item.direction}`);
+});
 }
-
 function printTravelLog(roverMars) {
   //Print travel lof of rove mars
 
   let i = 0;
+
   while (i < roverMars.travelLog.length) {
     console.log(
       `Direction: ${roverMars.travelLog[i + 2]}, Position: ${
@@ -339,6 +254,5 @@ function printTravelLog(roverMars) {
     );
     i += 3;
   }
+  console.log('\n');
 }
-
-function oldNNewPosition(copyX, copyY, roverMars) {}
